2ea25a30eb1cab8acac56c15a1cc56a8c23d2c2e
georgeorfanides <gorfanid@uwo.ca>
2020-09-22 16:45:52 -0400

    fix fic method

--- test.py
+++ test.py
@@ -1,22 +1,16 @@
 from decomposable_model import DecomposableModel
-import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
-import chordal as ch
+
+
 
 
 #build the decomposable model object with relevant data
 test = DecomposableModel("data/autos.csv")
 #learn the model using the kdg approach
 test.learn(k_max=4)
-test.learn(k_max=6, l_max=np.inf, max_time=1000.0, max_time_gurobi=500.0, maximal=False, coarsen=True,
-              extra_iters=2)
-#test.learn(k_max=np.inf, l_max=1, max_time=1000.0, max_time_gurobi=500.0, maximal=False, coarsen=True, extra_iters=2)
-test.coarsen()
 #convert learned (undirected) model to directed with minimal I-map
 test.to_bn()
 kdg_model = test.get_model_directed()
-test.coarsen()
+
 
 #relearn the model using the greedy approach
 test.greedy_learn(k_max=4)
